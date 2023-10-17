f8c903086da602e571135ff80c813571809dade1
Subash Paudel Chhetri <69474140+spyrochhetri@users.noreply.github.com>
2023-10-17 15:12:13 -0400

    Create dsaalgo.py

---
+++ dsaalgo.py
@@ -0,0 +1,92 @@
+# Bubble Sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Selection Sort
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+# Example usage:
+arr = [38, 27, 43, 3, 9, 82, 10]
+
+# Bubble Sort
+bubble_sort(arr.copy())
+print("Bubble Sort:", arr)
+
+# Selection Sort
+selection_sort(arr.copy())
+print("Selection Sort:", arr)
+
+# Insertion Sort
+insertion_sort(arr.copy())
+print("Insertion Sort:", arr)
+
+# Merge Sort
+merge_sort(arr.copy())
+print("Merge Sort:", arr)
+
+# Quick Sort
+sorted_arr = quick_sort(arr.copy())
+print("Quick Sort:", sorted_arr)
